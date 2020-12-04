412183b031126f362154d2879579254e58825db6
admin <admin@DESKTOP-9ETJQ6K>
2020-12-04 16:53:15 +0900

    2020-12-04 17:00
    -ajax 를 통한 샵 검색 TEST ( between 1 and 10 조건으로 변경)
    -return 결과값 확인 jquery를 통한 html로 append 필요

--- cndsalon/cndsalon/src/main/resources/static/js/shop/shop_search_main.js
+++ cndsalon/cndsalon/src/main/resources/static/js/shop/shop_search_main.js
@@ -1,4 +1,61 @@
+var mapContainer = document.getElementById('map');
+	var mapOption = {
+		center: new kakao.maps.LatLng(37.507586813960295, 127.02682191743735),
+		level: 3
+	};
+	var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+		
+		var positions = [
+		    {
+		        content: '<div>논현애완미용</div>', 
+		        latlng: new kakao.maps.LatLng(37.507586813960295, 127.02682191743735)
+		    },
+		    {
+		        content: '<div>커비애완미용</div>', 
+		        latlng: new kakao.maps.LatLng(37.50887075345535, 127.02678844593346)
+		    },
+		    {
+		        content: '<div>포이애완미용</div>', 
+		        latlng: new kakao.maps.LatLng(37.47887431342842, 127.04727510827242)
+		    },
+		    {
+		        content: '<div>나은애완미용</div>',
+		        latlng: new kakao.maps.LatLng(37.47913485668283, 127.04911246750096)
+		    }
+		];
 
+		for (var i = 0; i < positions.length; i ++) {
+		    // 마커를 생성합니다
+		    var marker = new kakao.maps.Marker({
+		        map: map, // 마커를 표시할 지도
+		        position: positions[i].latlng // 마커의 위치
+		    });
+
+		    // 마커에 표시할 인포윈도우를 생성합니다 
+		    var infowindow = new kakao.maps.InfoWindow({
+		        content: positions[i].content // 인포윈도우에 표시할 내용
+		    });
+
+		    // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+		    // 이벤트 리스너로는 클로저를 만들어 등록합니다 
+		    // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+		    kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+		    kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+		}
+
+		// 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+		function makeOverListener(map, marker, infowindow) {
+		    return function() {
+		        infowindow.open(map, marker);
+		    };
+		}
+
+		// 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+		function makeOutListener(infowindow) {
+		    return function() {
+		        infowindow.close();
+		    };
+		}
 
 /*
  init : function () {
