6705aa294e8f01dc4a38cd16330e7972b6c6a10f
happy-sonday <mrsmiler1106@gmail.com>
2020-12-05 23:19:53 +0900

    20.12.05 회원가입 폼 ajax 통신 및 유효성 관련 정규식 테스트진행
    -이메일 정규식 검증이 안됨(12.06 수정 예정)
    -휴대전화 본인인증 네이버 클라우드 플랫폼 'SENS' 등록은 했으나
    자바 스크립트 보안 객체 스터디 필요(SHA, HAC, CryptoJS 등)

---
+++ src/main/java/com/cndsalon/web/member/CertificationController.java
@@ -0,0 +1,79 @@
+package com.cndsalon.web.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+@RestController//JSON타입으로 데이터를 주고받아오면서 ResponseBobdy를 빼고 사용할 수 있다
+public class CertificationController {
+	
+	@PostMapping("/checkId")
+	public String checkId(@RequestBody Map<String, String> map) {
+				
+		String msg = "";		
+		if(map.get("id").equals("mrson1212")) {			
+			msg="<b style='color: red'>이미 가입된 아이디입니다.</b>";
+		}else {
+			msg="<b style='color: blue'>멋진 아이디에요!</b>";
+		}
+		return msg;	
+	}
+		
+
+	@PostMapping("/checkPhone")
+	public String checkPhone(@RequestBody Map<String, String> map) {
+		
+		
+		String msg = "";		
+		if(map.get("phone").equals("01096558120")) {
+			
+			msg="<b style='color: red'>이미 가입된 번호입니다."
+					+"<br />아이디 또는 비밀번호 찾기를 이용해 주세요"
+					+"</b>";
+		}
+		return msg;	
+	}
+	
+	@PostMapping("/checkEmail")
+	public String checkEmail(@RequestBody Map<String, String> map) {
+		
+		
+		String msg = "";		
+		
+		System.out.println(map.get("email"));
+		if(map.get("email").equals("son123@gmail.com")) {
+			msg="<b style='color: red'>이미 가입된 이메일입니다."
+					+"<br />아이디 또는 비밀번호 찾기를 이용해 주세요"
+					+"</b>";
+		}
+		return msg;	
+	}
+	
+	
+	
+	@PostMapping("/checkNickname")
+	public String checkNickname(@RequestBody Map<String, String> map) {
+		
+		
+		String msg = "";		
+		if(map.get("nickname").equals("왓섭")) {
+			
+			msg="<b style='color: red'>사용중인 별명입니다.</b>";
+		}else {
+			msg="<b style='color: blue'>사용 가능한 별명입니다.</b>";
+		}
+		return msg;	
+	}
+	
+		
+}
