8a23ed9811f18626261b697be0cd65a2fee669ec
admin <admin@DESKTOP-J1H3FKU>
2020-12-07 23:10:45 +0900

    2020-12-07 23:00
    -- ProjectConfig.java 추가--
    
    		registry.addResourceHandler("/static/**").
    		addResourceLocations("classpath:/static/");
    
    		registry.addResourceHandler("/upload_image/**").
    		addResourceLocations("file:///C:/data/upload_image/shop/");
    기존 파일 경로 변경 처리

--- src/main/java/com/cndsalon/repository/shop/CndSalonMapper.java
+++ src/main/java/com/cndsalon/repository/shop/CndSalonMapper.java
@@ -26,5 +26,10 @@ public interface CndSalonMapper {
 	@Select("SELECT * FROM shop_info_tbl WHERE s_code = #{sCode}")
 	CndSalonShopInfoVO getOne(@Param("sCode") String sCode);
 	
-	
+	/* 총 page 갯수 소숫점 자리 버림 처리
+	@Select("SELECT TRUNC(COUNT(*)/10) FROM\r\n"
+			+ "(SELECT DENSE_RANK() OVER (PARTITION BY A.S_CODE ORDER BY B.S_PHOTONAME) AS NO
+			 FROM SHOP_INFO_TBL A, SHOP_PHOTO_TBL B WHERE A.S_CODE = B.S_CODE(+))C\r\n"
+			+ "WHERE NO <2 ")
+	*/
 }
