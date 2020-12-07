2b0d38f3e37b7fa43a6bbd4bef71627da0e5a40a
admin <admin@DESKTOP-9ETJQ6K>
2020-12-07 14:03:11 +0900

    2020-12-07
    -- 마커 기능 활성화 --

---
+++ cndsalon/cndsalon/src/main/java/com/cndsalon/domain/shop/CndSalonShopDetailVO.java
@@ -0,0 +1,10 @@
+package com.cndsalon.domain.shop;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//매장상세정보 VO (대상 매장 정보 + 사진 + 디자이너정보 + 디자이너 사진 + 상품정보)
+public class CndSalonShopDetailVO {
+		
+}
---
+++ cndsalon/cndsalon/src/main/resources/static/js/shop/shop_search_my.js
@@ -0,0 +1,35 @@
+
+// 접속시 위치 정보를 허용 하면 hidden 값에 위치 정보를 저장 한다
+$(function () {        
+    // Geolocation API에 액세스할 수 있는지를 확인
+    if (navigator.geolocation) {
+        //위치 정보를 얻기
+        navigator.geolocation.getCurrentPosition (function(pos) {
+            //$('#latitude').html(pos.coords.latitude);     // 위도
+            $('#latitude_my_x').val(pos.coords.latitude);
+            //$('#longitude').html(pos.coords.longitude); // 경도
+            $('#latitude_my_y').val(pos.coords.longitude);
+        });
+    } else {// 거부 시 	
+	    alert("이 브라우저에서는 Geolocation이 지원되지 않습니다.")
+    }
+});
+// 버튼 클릭 시 내위치 정보를 넘겨 준다
+function shop_search(){
+	var url = "/cndsalon/getAll?";
+	var latitude_my_x = $('#latitude_my_x').val();
+	var latitude_my_y = $('#latitude_my_y').val();
+	
+		location.href= url+"userLocalX"+"="+latitude_my_x+
+		"&"+"userLocalY"+"="+latitude_my_y;		
+	
+	
+}
+// 버튼 클릭 시 search_ajax
+function shop_search_ajax(){
+	var url = "/cndsalon/getAll_ajax";
+	
+		location.href= url;		
+	
+	
+}
