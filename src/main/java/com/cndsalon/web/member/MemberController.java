04542ff2a922892f6681df2228fcf684fa3f3d2d
happy-sonday <mrsmiler1106@gmail.com>
2020-12-23 16:35:06 +0900

    12.23 gradlew test 점검 /home/ec2-user/app/step1/AnimalSalon/src/main/java/com/cndsalon/web/member/MemberController.java:4: error: package java.net.http does not exist import java.net.http.HttpRequest;

--- src/main/java/com/cndsalon/web/member/MemberController.java
+++ src/main/java/com/cndsalon/web/member/MemberController.java
@@ -1,25 +1,20 @@
 package com.cndsalon.web.member;
 
 
-import java.net.http.HttpRequest;
 
-import javax.servlet.RequestDispatcher;
+
+
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.annotation.RequestScope;
+
 
 import com.cndsalon.domain.member.DemoMember;
 import com.cndsalon.service.member.CertificationService;
