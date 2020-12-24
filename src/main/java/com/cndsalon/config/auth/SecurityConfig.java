4cddcce291de77630cf78bb1a404979febe29404
Chanwoo Lee <inulee112@naver.com>
2020-12-24 18:12:02 +0900

    payment with css_js_ing..

--- src/main/java/com/cndsalon/config/auth/SecurityConfig.java
+++ src/main/java/com/cndsalon/config/auth/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/",  "/static/**", "/member/**", "/check/**", "/shop/**", "/bookinghome/**", "/payments/**").permitAll()
+                    .antMatchers("/",  "/static/**", "/member/**", "/check/**", "/shop/**", "/bookinghome/**", "/payments/**","/booking/**").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.CLIENT.name())
                     .anyRequest().authenticated()
                 .and()
