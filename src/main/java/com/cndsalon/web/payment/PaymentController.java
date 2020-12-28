2c543a6b5ed73745d27273fda7df7c6a5af6f98c
Chanwoo Lee <inulee112@naver.com>
2020-12-28 17:42:24 +0900

    pay & book connect&DB insert

--- src/main/java/com/cndsalon/web/payment/PaymentController.java
+++ src/main/java/com/cndsalon/web/payment/PaymentController.java
@@ -1,5 +1,9 @@
 package com.cndsalon.web.payment;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -60,7 +64,8 @@ public class PaymentController {
 			@RequestParam("defaultBeautyTime") Integer workingTime,
 			@RequestParam("selected_designer") String dCode,
 			@RequestParam("bookingDate") String bDate,
-			@RequestParam("time_radio") String bTime) throws Exception{
+			@RequestParam("time_radio") String bTime
+			) throws Exception{
 		ModelAndView mv = new ModelAndView("/payment/paymentForm");
 		log.info("결제요청을 받음.. 로직 수행 시작");
 		log.info("메뉴코드 : " + mCode);
@@ -71,13 +76,25 @@ public class PaymentController {
 		log.info("소요시간 : " + workingTime);
 		log.info("금액 : " + price);
 		
-		BookingView result = bookingService.getBookingView(sCode,mCode, dCode);
+		String refineDate = bDate.substring(0, bDate.length()-3);
+		log.info("정제된 날짜값 : " + refineDate);
+//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//		LocalDate bookingDate = LocalDate.parse(refineDate, formatter);
+//		log.info("LocalDate : "  + bookingDate);
 		
+//		LocalTime bookingTime = LocalTime.parse(bTime);
+//		log.info("LocalTime : " + bookingTime);
+		
+		BookingView result = bookingService.getBookingView(sCode,mCode, dCode);
+		String id = "testID";
 		mv.addObject("result", result);
-		mv.addObject("bookingDate", bDate);
-		mv.addObject("bookingTime", bTime);
+		mv.addObject("bookingDateText", bDate);
+		mv.addObject("bookingTimeText", bTime);
+		mv.addObject("bookingDate", refineDate);
+//		mv.addObject("bookingTime", bookingTime);
 		mv.addObject("workingTime", workingTime);
 		mv.addObject("price", price);
+		mv.addObject("id", id);
 		
 		return mv;
 	}
