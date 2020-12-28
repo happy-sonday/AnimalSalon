9ec2bb79eadb98ea941fb2e6252e12863684f2a1
Chanwoo Lee <inulee112@naver.com>
2020-12-28 18:22:28 +0900

    payment commit_2020-12-28

--- src/main/java/com/cndsalon/web/payment/PaymentController.java
+++ src/main/java/com/cndsalon/web/payment/PaymentController.java
@@ -1,12 +1,6 @@
 package com.cndsalon.web.payment;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
